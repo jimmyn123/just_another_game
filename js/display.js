2c9dfd98426fdcddb08bdc6fc2bcbdbb2e79b3f3
neth <nethwebdev@gmail.com>
2018-08-21 14:28:26 -0700

    started script for picking player to answer

--- js/display.js
+++ js/display.js
@@ -1,3 +1,5 @@
+var activePlayer = 1;
+
 // generate player boxes
 var playerScoreArea = document.getElementById('player-score-area');
 var playerData = JSON.parse(localStorage.getItem('playerArr'));
@@ -20,12 +22,84 @@ playerArr.forEach(function(player) {
   playerScoreArea.appendChild(newDiv);
 });
 
-// dynamically set the font size for lengthier answers
-var answerDivs = document.getElementsByClassName('individual-answers');
+// 
 
-for (var i = 0; i < answerDivs.length; i++) {
-  answerDivs[i].classList.remove('small-answer-text');
-  if (answerDivs[i].innerText.length > 14) {
-    answerDivs[i].classList.add('small-answer-text');
+// get questions
+var questionText = document.getElementById('question-text');
+var questionHead = document.getElementById('question-head');
+var answerDiv = document.getElementById('answer-div');
+var questionPool = [];
+var usedQuestions = [];
+
+function getRandomQuestion() {
+  var qNum = Math.floor(Math.random() * allQuestions.length);
+  while (usedQuestions.includes(qNum)) {
+    qNum = Math.floor(Math.random() * allQuestions.length);
+  }
+  var selectedQuestion = allQuestions[qNum];
+  usedQuestions.push(qNum);
+  questionHead.innerText = `Here is your next category:`
+  questionText.innerText = `${selectedQuestion.category}`;
+  createPickerButton(selectedQuestion);
+}
+
+function clearAnswers() {
+  while(answerDiv.firstChild) {
+    answerDiv.removeChild(answerDiv.firstChild);
   }
 }
+
+function createPickerButton(question) {
+  clearAnswers();
+  for (var i = 0; i < playerData.length; i++) {
+    var newDiv = document.createElement('div');
+    newDiv.classList.add('individual-answers');
+    var newP = document.createElement('p');
+    newDiv.classList.add('small-answer-text');
+    newP.innerText = `Make ${playerData[i]} answer.`;
+    newDiv.appendChild(newP);
+    answerDiv.appendChild(newDiv);
+    newDiv.addEventListener('click', function() {
+      displayQuestion(question);
+    });
+  }
+}
+
+function displayQuestion(question) {
+  questionHead.innerText = 'Question:';
+  questionText.innerText = question.question;
+  populateAnswers(question.answers);
+  adjustAnswerText();
+}
+
+function populateAnswers(answers) {
+  clearAnswers();
+  var scrambledAnswers = [];
+  var answerIndex = Math.floor(Math.random() * answers.length);
+  for (var i = 0; i < answers.length; i++) {
+    while (scrambledAnswers.includes(answerIndex)) {
+      answerIndex = Math.floor(Math.random() * answers.length);
+    }
+    scrambledAnswers.push(answerIndex);
+    var newAnswer = document.createElement('div');
+    newAnswer.classList.add('individual-answers');
+    var newPTag = document.createElement('p');
+    newPTag.innerText = answers[answerIndex];
+    newAnswer.appendChild(newPTag);
+    answerDiv.appendChild(newAnswer);
+  }
+}
+
+// dynamically set the font size for lengthier answers
+function adjustAnswerText() {
+  var answerDivs = document.getElementsByClassName('individual-answers');
+
+  for (var i = 0; i < answerDivs.length; i++) {
+    answerDivs[i].classList.remove('small-answer-text');
+    if (answerDivs[i].innerText.length > 14) {
+      answerDivs[i].classList.add('small-answer-text');
+    }
+  }
+}
+
+getRandomQuestion();
