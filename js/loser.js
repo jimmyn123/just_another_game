680ad61d9aaec54633e533ee78f1e15618173e91
neth <nethwebdev@gmail.com>
2018-08-22 13:48:07 -0700

    loser page and logic is working

--- js/loser.js
+++ js/loser.js
@@ -1,14 +1,23 @@
-var h1 = document.getElementById('name');
 var loser = JSON.parse(localStorage.getItem('loser'));
 var numberOfPlayers = parseInt(localStorage.getItem('totalPlayers'));
-var names = '';
-loser.forEach(function(loser) {
-  names += loser.name + ' '
-});
-if (numberOfPlayers === loser.length) {
-  h1.innerText = 'EVERYBODY LOSES!!!!';
+var names = [];
+var pTags = document.getElementsByTagName('p');
+console.log(loser.length, numberOfPlayers);
+console.log(loser.length === numberOfPlayers);
+
+if (loser.length === numberOfPlayers) {
+  pTags[0].innerText = 'Holy CRAP!';
+  pTags[1].innerText = 'We have a house full of losers today.';
+  pTags[2].innerText = 'Nobody wins!';
 } else if (loser.length > 1) {
-  h1.innerText = `Today\'s losers: ${names}`;
+  loser.forEach(function(player) {
+    names.push(player.name);
+  });
+  pTags[0].innerText = 'Here are today\'s losers:';
+  pTags[1].innerText = names.join(' and ');
+  pTags[2].innerText = 'You can reflect on your failure together.';
 } else {
-  h1.innerText = `Today\'s loser is ${names}`;
+  pTags[0].innerText = `${loser[0].name}, Remember today...`;
+  pTags[1].innerText = 'It is the day you have lost!';
+  pTags[2].innerText = 'You Got Played!!!';
 }
